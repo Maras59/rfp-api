1931968c0b1b0985d8011665cd7419e5910e2f43
Kaleb Smith <kalebsmith11@gmail.com>
2024-02-05 17:56:49 -0700

    feature: implement milvus init

--- rfp_api/source/milvus_index.py
+++ rfp_api/source/milvus_index.py
@@ -1,8 +1,22 @@
 from collections import defaultdict
+from contextlib import suppress
+from dataclasses import dataclass
 from typing import List, Optional, Tuple
-from pymilvus import Collection, connections
+
+import pandas as pd
 import sentence_transformers
+from pymilvus import Collection, CollectionSchema, DataType, FieldSchema, connections
+
 from .model import QueryResult
+
+
+@dataclass
+class MilvusConnectionSecrets:
+    alias: Optional[str] = "default"
+    user: str
+    password: str
+    host: Optional[str] = "localhost"
+    port: Optional[str] = "19530"
 
 
 class MilvusService:
@@ -12,22 +26,46 @@ class MilvusService:
         "offset": 0,
         "ignore_growing": False,
     }
+    collection_name = "questions"
+    index_name = "questions_embedding"
 
-    def __init__(self):
-        self.connection = connections.connect(
-            alias="default", user="username", password="password", host="localhost", port="19530"
-        )
-        self.collection: Collection = None
-        self.embedded_field_name: str = None
+    def __init__(self, credentials: MilvusConnectionSecrets, df: Optional[pd.DataFrame] = None, verbose: bool = False):
+        connections.connect(**credentials.__dict__)
+        self.collection: Collection = self.create_or_get_collection()
+        if df is not None:
+            self.insert(df)
+        self.create_index()
+        self.verbose = verbose
         self.embedding_model = sentence_transformers("all-MiniLM-L6-v2")
+
+    def create_or_get_collection(self) -> Collection:
+        with suppress(Exception):
+            return Collection(self.collection_name)
+        question_id = FieldSchema(
+            name="id",
+            dtype=DataType.INT64,
+            is_primary=True,
+        )
+        text_embedding = FieldSchema(name=self.index_name, dtype=DataType.FLOAT_VECTOR, dim=768)
+        schema = CollectionSchema(
+            fields=[question_id, text_embedding], description="Question search", enable_dynamic_field=True
+        )
+
+        return Collection(name=self.collection_name, schema=schema, using="default", shards_num=2)
+
+    def create_index(self):
+        self.collection.create_index(field_name=self.index_name, index_params=self.index_params)
+
+    def insert(self, df: pd.DataFrame):
+        self.collection.insert(df)
 
     def query(self, query: str, k: Optional[int] = 10, threshold: float = float("inf")) -> List[QueryResult]:
         if k > self.collection.count():
             raise ValueError(f"Your index has size {self.collection.count} but you set n_results to {k}.")
 
-        embedding = self.embedding_model.encode([query])
+        vector = self.embedding_model.encode([query])
         query_results = self.collection.search(
-            data=embedding, anns_field=self.embedded_field_name, param=self.search_params, limit=k
+            data=vector, anns_field=self.embedded_field_name, param=self.search_params, limit=k, expr=None
         )
         results = []
         for result in query_results:
@@ -65,4 +103,4 @@ class MilvusService:
         return top_answers
 
     def __sizeof__(self) -> int:
-        return self.collection.count()
+        return self.collection.num_entities
