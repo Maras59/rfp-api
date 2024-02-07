4c0610e6602baaef3d98245980602db04807c9c6
Kaleb Smith <kalebsmith11@gmail.com>
2024-02-06 20:04:13 -0700

    feature: write schema in models.py
    https://ppcapstone.atlassian.net/browse/SCRUM-124

--- rfp_api/api/models.py
+++ rfp_api/api/models.py
@@ -1,7 +1,28 @@
 from django.db import models
+from django.db import models
 
-# Create your models here.
 
-# class Person(models.Model):
-#     first_name = models.CharField(max_length=30)
-#     last_name = models.CharField(max_length=30)
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    owner_organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Question(models.Model):
+    text = models.TextField()
+    is_active = models.BooleanField(default=True)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
