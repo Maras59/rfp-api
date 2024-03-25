d93ec7d73c2b683dea2f3efdff6764a306d53694
Maras59 <marcosespino103@gmail.com>
2024-03-25 00:56:14 -0600

    Remove unused imports

--- server/rfp_api/admin.py
+++ server/rfp_api/admin.py
@@ -4,7 +4,6 @@ from django.utils.html import format_html
 from django.utils.http import urlencode
 from django.utils.translation import gettext_lazy as _
 from .models import Answer, Organization, Question
-from django import forms
 
 
 # Register your models here.
